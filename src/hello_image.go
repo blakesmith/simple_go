9f7a105522355feea685b242cdf5dfc7db17dfb9
Blake Smith <blakesmith0@gmail.com>
2012-10-03 22:28:25 -0500

    Add return statements

--- src/hello_image.go
+++ src/hello_image.go
@@ -11,12 +11,16 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	file, err := os.Open("hello_golang.png")
 	if err != nil {
 		log.Fatal(err)
+
+		return
 	}
 	defer file.Close()
 
 	b, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Fatal(err)
+
+		return
 	}
 
 	w.Header().Set("Content-type", "image/png")
