77d5bdd8e569fdcc98b7f196cf77d7a17c910caf
Blake Smith <blakesmith0@gmail.com>
2012-10-03 22:23:55 -0500

    Add 'hello image' example

---
+++ src/hello_image.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"io/ioutil"
+	"os"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("hello_golang.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-type", "image/png")
+	w.Write(b)
+}
+
+func main() {
+	log.Println("Listening for requests...")
+
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":5555", nil)
+}
